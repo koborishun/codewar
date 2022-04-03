f3250c7afed57f30ec11c865cc49c0b963841f5f
koborishun <koboshun0723@icloud.com>
2022-04-03 10:02:15 +0900

    引数にString型を受け取り、受け取った文字列の中から特定の文字をある文字に変換して返す関数

---
+++ codewar/src/seven/ComplementaryDNA.java
@@ -0,0 +1,57 @@
+package seven;
+
+//Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+//
+//If you want to know more: http://en.wikipedia.org/wiki/DNA
+//
+//In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+//
+//More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+//
+//Example: (input --> output)
+
+//"ATTGC" --> "TAACG"
+//"GTAT" --> "CATA"
+public class ComplementaryDNA {
+	public static void main(String[] args) {
+		System.out.println(makeComplement("AAAA"));
+		System.out.println(makeComplement("ATTGC"));
+		System.out.println(makeComplement("GTAT"));
+	}
+
+	public static String makeComplement(String dna) {
+		//Your code
+		dna = dna.toUpperCase();
+		char[] array = dna.toCharArray();
+		for (int i = 0; i < array.length; i++) {
+			if ('A' == array[i]) {
+				array[i] = 'T';
+			} else if ('T' == array[i]) {
+				array[i] = 'A';
+			} else if ('C' == array[i]) {
+				array[i] = 'G';
+			} else {
+				array[i] = 'C';
+			}
+		}
+		return String.valueOf(array);
+	}
+	//解答例1
+	//	char[] chars = dna.toCharArray();
+	//    for(int i = 0; i < chars.length; i++) {
+	//      chars[i] = getComplement(chars[i]);
+	//    }
+	//
+	//    return new String(chars);	
+	//  }
+	//
+	//  private static char getComplement(char c) {
+	//    switch(c) {
+	//      case 'A': return 'T';
+	//      case 'T': return 'A';
+	//      case 'C': return 'G';
+	//      case 'G': return 'C';
+	//    }
+	//    return c;
+	//  }
+}
