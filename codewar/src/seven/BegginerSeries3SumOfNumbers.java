7f4eff45cd2d15e85523f2f46f8cf6df6a96ffad
koborishun <koboshun0723@icloud.com>
2022-05-02 21:52:21 +0900

    引数にint型aとbをとり、aからbまでを足し合わせた結果をint型として返す関数

---
+++ codewar/src/seven/BegginerSeries3SumOfNumbers.java
@@ -0,0 +1,39 @@
+package seven;
+
+//Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+//
+//Note: a and b are not ordered!
+//
+//Examples (a, b) --> output (explanation)
+//
+//(1, 0) --> 1 (1 + 0 = 1)
+//(1, 2) --> 3 (1 + 2 = 3)
+//(0, 1) --> 1 (0 + 1 = 1)
+//(1, 1) --> 1 (1 since both are same)
+//(-1, 0) --> -1 (-1 + 0 = -1)
+//(-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+public class BegginerSeries3SumOfNumbers {
+	public static void main(String[] args) {
+		System.out.println(GetSum(1, 0));
+		System.out.println(GetSum(1, 2));
+		System.out.println(GetSum(0, 1));
+		System.out.println(GetSum(1, 1));
+		System.out.println(GetSum(-1, 0));
+		System.out.println(GetSum(-1, 2));
+	}
+
+	//不正解
+	public static int GetSum(int a, int b) {
+		//Good luck!
+		return (int) ((double) a + b) / 2 * Math.max(a, b);
+	}
+	//解答例1
+	//     public int GetSum(int a, int b)  {
+	//        return (a + b) * (Math.abs(a - b) + 1) / 2;
+	//     }
+	//解答例2
+	//IntStreamのrange(int start, int end) 	開始値以上、終了値未満の連続する整数を返す
+	//     public int GetSum(int a, int b) {
+	//        return IntStream.range(Math.min(a, b), Math.max(a, b) + 1).sum();
+	//     }
+}
