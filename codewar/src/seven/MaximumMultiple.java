6b89f7eead65236102d2575f35f72496926928c1
koborishun <koboshun0723@icloud.com>
2022-05-30 20:22:01 +0900

    引数にint型(divisor 割る数:bound 範囲内の最大値)をとり、divisorで割れる最大値を返す関数

---
+++ codewar/src/seven/MaximumMultiple.java
@@ -0,0 +1,47 @@
+package seven;
+//Task
+
+//Given a Divisor and a Bound , Find the largest integer N , Such That ,
+//
+//Conditions :
+//N is divisible by divisor
+//N is less than or equal to bound
+//N is greater than 0.
+//
+//Notes
+//The parameters (divisor, bound) passed to the function are only positive values .
+//It's guaranteed that a divisor is Found .
+//Input >> Output Examples
+//maxMultiple (2,7) ==> return (6)
+//Explanation:
+//(6) is divisible by (2) , (6) is less than or equal to bound (7) , and (6) is > 0 .
+//maxMultiple (10,50)  ==> return (50)
+//Explanation:
+//(50) is divisible by (10) , (50) is less than or equal to bound (50) , and (50) is > 0 .*
+//maxMultiple (37,200) ==> return (185)
+//Explanation:
+//(185) is divisible by (37) , (185) is less than or equal to bound (200) , and (185) is > 0 .
+
+public class MaximumMultiple {
+	public static void main(String[] args) {
+		System.out.println(maxMultiple(2, 7));
+		System.out.println(maxMultiple(10, 50));
+		System.out.println(maxMultiple(17, 200));
+	}
+
+	public static int maxMultiple(int divisor, int bound) {
+		// Your code here
+		int a = bound / divisor;
+		return a *= divisor;
+	}
+
+	//解答例1
+	//	public static int maxMultiple(int divisor, int bound) {
+	//		return bound - (bound % divisor);
+	//	}
+
+	//解答例2
+	//	public static int maxMultiple(int divisor, int bound) {
+	//		return bound / divisor * divisor;
+	//	}
+}
